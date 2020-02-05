dc397e9b3a0e8bd2ef826d4e47fed5773dacee79
off-art <art.dev.k@gmail.com>
2020-02-05 19:46:46 +0300

    ref js

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ window.onload = function() {
   let list = document.querySelector(".todo-list");
   let button = document.querySelector(".todo-button");
 
-  // Создание нового элемента списка
-  button.addEventListener("click", function() {
+  function addTask() {
     if (text.value == "") {
       return text.value;
     } else {
@@ -14,38 +13,25 @@ window.onload = function() {
       itemList.innerText = text.value;
       list.appendChild(itemList);
       buttonClose.className = "fa fa-trash-o de todo-list__item_close";
-
       itemList.appendChild(buttonClose);
       text.value = "";
+
       buttonClose.addEventListener("click", function(e) {
         e.target.offsetParent.style.display = "none";
       });
+
       itemList.addEventListener("click", function(e) {
         e.target.classList.toggle("closeTask");
       });
     }
-  });
-  document.addEventListener("keypress", function(e) {
-    if (text.value == "") {
-      return text.value;
-    } else {
-      if (e.charCode === 13) {
-        let itemList = document.createElement("div");
-        let buttonClose = document.createElement("div");
-        itemList.className = "todo-list__item";
-        itemList.innerText = text.value;
-        list.appendChild(itemList);
-        buttonClose.className = "fa fa-trash-o de todo-list__item_close";
+  }
 
-        itemList.appendChild(buttonClose);
-        text.value = "";
-        buttonClose.addEventListener("click", function(e) {
-          e.target.offsetParent.style.display = "none";
-        });
-        itemList.addEventListener("click", function(e) {
-          e.target.classList.toggle("closeTask");
-        });
-      }
-    }
+  // Создание нового элемента списка
+  button.addEventListener("click", function() {
+    addTask()
+  });
+  
+  document.addEventListener("keypress", function(e) {
+    addTask()
   });
 };
